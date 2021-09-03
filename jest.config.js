43d5d67875c8e8e8125e74b66ec90e338c352fa2
elian <elian_491@hotmail.com>
2021-09-03 19:31:49 -0300

    chore: add jest config

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    testEnvironment: "node",
+    moduleFileExtensions: ['js', 'ts'],
+    modulePathIgnorePatterns: ['<rootDir>/src/interfaces'],
+    roots: [
+        "<rootDir>/src"
+    ],
+}
