fe9fbfdeb9340d8dcec9f480b2b863530f05ec6d
elian <elian_491@hotmail.com>
2021-09-03 20:06:33 -0300

    test: ensure registerUser return 400 if username is not provide

--- src/useCases/registerUser/registerUser.spec.ts
+++ src/useCases/registerUser/registerUser.spec.ts
@@ -1,11 +1,12 @@
 
 class registerUser {
     handle(httpRequest: any) {
-        if(!httpRequest.body.email){
+        if(!httpRequest.body.email || !httpRequest.body.username){
             return {
                 statusCode: 400
             }
         }
+       
     }
 }
 
@@ -15,6 +16,19 @@ describe('Register user', () => {
         const sut = new registerUser()
         const httpRequest = {
             body: {
+                username: 'any_username',
+                password: 'any_password'
+            }
+        }
+        const httpResponse : any = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+    }),
+
+    test('should return 400 if username is not provided', () => {
+        const sut = new registerUser()
+        const httpRequest = {
+            body: {
+                email: 'any_email',
                 password: 'any_password'
             }
         }
