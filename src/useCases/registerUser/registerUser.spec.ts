6b65f96b9b120aaadd51aefb0f6b218893c927a2
elian <elian_491@hotmail.com>
2021-09-03 21:36:29 -0300

    test: ensure register user return custom error

--- src/useCases/registerUser/registerUser.spec.ts
+++ src/useCases/registerUser/registerUser.spec.ts
@@ -1,5 +1,7 @@
 import { IHttpRequest , IHttpResponse } from '../../interfaces/IHttp';
- 
+import { MissingParamater } from '../../errors/clientError'
+
+
 class registerUser {
     handle(httpRequest: IHttpRequest) {
         const requiredProperties = ["username", "email", "password"];
@@ -7,12 +9,13 @@ class registerUser {
             if(!httpRequest.body[props]){
                 return {
                     statusCode: 400,
-                    body: new Error("bad request")
+                    body: new MissingParamater(props)
                 }
             }
         }
     }
 }
+
 
 
 describe('Register user', () => {
@@ -26,6 +29,7 @@ describe('Register user', () => {
         }
         const httpResponse : IHttpResponse = sut.handle(httpRequest)!
         expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new MissingParamater('email'))
     }),
 
     test('should return 400 if username is not provided', () => {
@@ -38,6 +42,20 @@ describe('Register user', () => {
         }
         const httpResponse : IHttpResponse = sut.handle(httpRequest)!
         expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new MissingParamater('username'))
+    })
+
+    test('should return 400 if password is not provided', () => {
+        const sut = new registerUser()
+        const httpRequest = {
+            body: {
+                username: 'any_username',
+                email: 'any_email'
+            }
+        }
+        const httpResponse : IHttpResponse = sut.handle(httpRequest)!
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new MissingParamater('password'))
     })
     
 })
