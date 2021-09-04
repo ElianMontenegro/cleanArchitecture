1c777fdeb03c65e0915e5ba95c6e3cb96bb1d08e
elian <elian_491@hotmail.com>
2021-09-03 22:57:28 -0300

    feat: add DTO interface for data use case

---
+++ src/interfaces/IRegisterUseCase.ts
@@ -0,0 +1,10 @@
+export interface IRegisterUserDTO {
+    username: string;
+    email: string;
+    password: string;
+    repeatPassword: string;
+}
+
+export interface IRegisterUseCase{
+    register(user: IRegisterUserDTO) : any
+}
