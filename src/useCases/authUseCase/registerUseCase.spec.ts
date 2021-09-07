b9510ed6fc3daf247c3c2bd12c401bb929205c3c
elian <elian_491@hotmail.com>
2021-09-07 13:57:10 -0300

    test: ensure RegisterUseCase retrun error if username,password and repeatPassword are empty

--- src/useCases/authUseCase/registerUseCase.spec.ts
+++ src/useCases/authUseCase/registerUseCase.spec.ts
@@ -5,6 +5,15 @@ export class RegisterUseCase implements IRegisterUseCase {
         if(!user.email){
             return new Error("email")
         }
+        if(!user.username){
+            return new Error("username")
+        }
+        if(!user.password){
+            return new Error("password")
+        }
+        if(!user.repeatPassword){
+            return new Error("repeatPassword")
+        }
     }
 }
 describe('RegisterUseCase', () => {
@@ -16,6 +25,34 @@ describe('RegisterUseCase', () => {
         let repeatPassword = ""
         const  res = await sut.register({email,username,password, repeatPassword})
         expect(res).toEqual(new Error("email"))
+    })
+
+    test('should return Error if username is empty',async () => {
+        const sut = new RegisterUseCase()
+        let email = "any_email"
+        let username = ""
+        let password = ""
+        let repeatPassword = ""
+        const  res = await sut.register({email,username,password, repeatPassword})
+        expect(res).toEqual(new Error("username"))
+    })
+    test('should return Error if password is empty',async () => {
+        const sut = new RegisterUseCase()
+        let email = "any_email"
+        let username = "any_username"
+        let password = ""
+        let repeatPassword = ""
+        const  res = await sut.register({email,username,password, repeatPassword})
+        expect(res).toEqual(new Error("password"))
+    })
+    test('should return Error if repeatPassword is empty',async () => {
+        const sut = new RegisterUseCase()
+        let email = "any_email"
+        let username = "any_username"
+        let password = "any_password"
+        let repeatPassword = ""
+        const  res = await sut.register({email,username,password, repeatPassword})
+        expect(res).toEqual(new Error("repeatPassword"))
     })
     
 })
