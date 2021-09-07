907891e9521c6a91b5da8a9c9efa054245b6edf6
elian <elian_491@hotmail.com>
2021-09-07 16:54:03 -0300

    refactor: move the production class EmailValidator to their own file

---
+++ src/utils/emailValidator.ts
@@ -0,0 +1,21 @@
+
+export class EmailValidator {
+    public isValid(email: string): boolean{
+        const emailRegex = /^[-!#$%&'*+\/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
+        if (!email)
+            return false;
+    
+        if(email.length>254)
+            return false;
+    
+        let valid = emailRegex.test(email);
+        if(!valid)
+            return false;
+    
+        let parts = email.split("@");
+        if(parts[0].length>64)
+            return false;
+        
+        return true;
+    }
+}
