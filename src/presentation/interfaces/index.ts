13abe528af65ac002f2a229fbf304275b916ff9b
elian <elian_491@hotmail.com>
2021-09-11 15:52:30 -0300

    refactor: import interface to index can export call of index

---
+++ src/presentation/interfaces/index.ts
@@ -0,0 +1,12 @@
+import { IComparePassword } from './IComparePassword'
+import { IEmailValidator } from './IEmailValidator'
+import { IEncrypter } from './IEncrypter'
+import { IUserModel } from './IUserModel'
+import { IJwt } from './IJwt'
+export {
+    IComparePassword,
+    IEmailValidator,
+    IEncrypter,
+    IUserModel,
+    IJwt
+}
