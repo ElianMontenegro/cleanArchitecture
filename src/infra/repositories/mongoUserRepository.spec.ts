d4addbed0c4f14b5bcc1504e167cb2d14ddba730
elian <elian_491@hotmail.com>
2021-09-15 14:35:56 -0300

    test: ensure mongoUserRepository save() return an user if user created

--- src/infra/repositories/mongoUserRepository.spec.ts
+++ src/infra/repositories/mongoUserRepository.spec.ts
@@ -3,7 +3,16 @@ import { userModel } from "../model/userSchema"
 import { MongoUserRepository } from "./mongoUserRepository"
 
 const makeSut = () => {
-    return new MongoUserRepository(userModel); 
+    const userFake = new userModel({
+        email : "emailExist@gmail.com",
+        username : "any_username",
+        password: "any_password"
+    })
+    const sut = new MongoUserRepository(userModel); 
+    return {
+        sut,
+        userFake
+    }
 }
 
 
@@ -21,24 +30,23 @@ describe('mongoUserRepository', () => {
     });
 
     test('should return null if user is not found',async () => {
-        const sut = makeSut()
+        const { sut } = makeSut()
         const user = await sut.loadUserByEmail("emailDontExist@gmail.com");
         expect(user).toBeNull();
     })
 
     test('should return an user if user is found',async () => {
-        const sut = makeSut()
-        const userFake = new userModel({
-            email : "emailExist@gmail.com",
-            username : "any_username",
-            password: "any_password"
-        })
+        const { sut, userFake } = makeSut()
         const userFakeSave = await userFake.save();
         const user = await sut.loadUserByEmail("emailExist@gmail.com");
         expect(user.email).toBe(userFakeSave.email)
     })
     
-    
+    test('should return an user  if user created',async () => {
+        const { sut, userFake } = makeSut()
+        const user = await sut.save(userFake);
+        expect(userFake).toEqual(user);
+    })
     
     
 })
