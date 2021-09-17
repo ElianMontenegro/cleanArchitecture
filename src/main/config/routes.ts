25a431bd32114d9c6ee5e69958f85f2709c2ff3e
elian <elian_491@hotmail.com>
2021-09-16 21:59:14 -0300

    feat: create file export routes with fast-glob

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -4,5 +4,5 @@ export default (app: Express): void => {
     const router = Router();
     app.use('/api', router);
     //config to import routes automatically
-    fg.sync('**/src/main/routes/**router.ts').map(async file => (await import(`../../../${file}`)).default(router))
+    fg.sync('**/src/main/routes/**routes.ts').map(async file => (await import(`../../../${file}`)).default(router))
 }
