bdf88a87d150170a1adbf15062d340d26bb3e8ca
elian <elian_491@hotmail.com>
2021-09-17 11:22:13 -0300

    refactor: create  makeSut factorie with sut and user

--- src/presentation/controllers/loginUserController.spec.ts
+++ src/presentation/controllers/loginUserController.spec.ts
@@ -1,22 +1,34 @@
 import { badRequest } from "../helpers/httpError";
 import { LoginUserController } from './loginUserController'
 
-
+const makeSut = () => {
+    const user = { 
+        email : 'any_email',
+        password : 'any_password'
+    }
+    const sut = new LoginUserController()
+    return {
+        sut,
+        user
+    }
+    
+}
 
 describe('LoginUserController', () => {
     test('should return 400 if email is not provided', async () => {
-        const sut = new LoginUserController();
-        const httpRequest = { body : { email : ''} }
+        const { sut, user }= makeSut()
+        user.email = ''
+        const httpRequest = { 
+            body : user 
+        }
         const res = await sut.handle(httpRequest)
         expect(res).toStrictEqual(badRequest('email'))
     })
     test('should return 400 if password is not provided', async () => {
-        const sut = new LoginUserController();
+        const { sut, user }= makeSut()
+        user.password = ''
         const httpRequest = { 
-            body : { 
-                email : 'any_email',
-                password : ''
-            } 
+            body : user 
         }
         const res = await sut.handle(httpRequest)
         expect(res).toStrictEqual(badRequest('password'))
