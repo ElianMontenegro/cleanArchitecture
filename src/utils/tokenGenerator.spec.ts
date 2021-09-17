ac933edf2d6d8678cad90b277868786165ce7c7f
elian <elian_491@hotmail.com>
2021-09-16 22:03:41 -0300

    test: ensure token Generator return null

--- src/utils/tokenGenerator.spec.ts
+++ src/utils/tokenGenerator.spec.ts
@@ -2,8 +2,8 @@
 import { TokenGenerator } from './tokenGenerator'
 
 class TokenGeneratorSpy {
-    jwtToken : any
-    token(id : string): String{
+    jwtToken = null
+    token(id : string): String | null{
         return this.jwtToken
     }
 }
@@ -12,9 +12,7 @@ class TokenGeneratorSpy {
 describe('tokenGenerator', () => {
     test('should return null if token if null', () => {
         const sut = new TokenGeneratorSpy();
-        const token = sut.token('id_user');
-        sut.jwtToken = null
+        const token = sut.token('id_user')
         expect(token).toBeNull()
     })
-
 })
