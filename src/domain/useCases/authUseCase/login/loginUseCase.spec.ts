abf3a85d6ec82411902bd7d00167d8961ab35f0e
unknown <elianmontenegro491@gmail.com>
2021-09-18 16:25:44 -0300

    test: ensure return badRequest error if email is invalid

--- src/domain/useCases/authUseCase/login/loginUseCase.spec.ts
+++ src/domain/useCases/authUseCase/login/loginUseCase.spec.ts
@@ -1,12 +1,23 @@
 import { badRequest } from "../../../../presentation/helpers/httpError";
 import { LoginUseCases } from './loginUseCase'
 
+const makeEmailValidator = () => {
+    class EmailValidatorSpy{
+        valid : any
+        isValid(email: string){
+            return this.valid
+        }
+    }
+    return new EmailValidatorSpy()
+}
+
 const makeSut = () => {
     const user = {
         email : "any_email",
         password : "any_password"
     }
-    const sut = new LoginUseCases()
+    const emailValidatorSpy = makeEmailValidator()
+    const sut = new LoginUseCases(emailValidatorSpy)
     return {
         user,
         sut
@@ -38,6 +49,17 @@ describe('LoginUseCase', () => {
             throwError = error
         }
         expect(throwError).toEqual(badRequest('password'))
+    })
+
+    test("should return badRequest error if email is invalid",async () => {
+        const { sut, user} = makeSut();
+        let throwError 
+        try {
+            await sut.login(user)
+        } catch (error) {
+            throwError = error
+        }
+        expect(throwError).toEqual(badRequest('email is not valid'))
     })
 
 })
