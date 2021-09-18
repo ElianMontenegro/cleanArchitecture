bd6481e783631270fca5765c777aa848c2f990f9
unknown <elianmontenegro491@gmail.com>
2021-09-18 16:27:21 -0300

    feat: use EmailValidat by dependency injectio in LoginUseCase

--- src/domain/useCases/authUseCase/login/loginUseCase.ts
+++ src/domain/useCases/authUseCase/login/loginUseCase.ts
@@ -1,13 +1,18 @@
 import { badRequest } from "../../../../presentation/helpers/httpError";
+import { IEmailValidator } from "../../../../presentation/interfaces";
 import { ILoginUseCase, ILoginUserDTO } from "./ILoginUseCase";
 
 
 export class LoginUseCases implements ILoginUseCase{
+    constructor(private readonly emailValidator : IEmailValidator){}
     async login(user: ILoginUserDTO): Promise<any>{
         for (const [key, value] of Object.entries(user)) {
             if (!value) {
                 throw badRequest(key);
             }
+        }
+        if(!this.emailValidator.isValid(user.email)){
+            throw badRequest("email is not valid")
         }
         
     }
