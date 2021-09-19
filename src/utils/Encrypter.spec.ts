eec39107195d6233a9dfc2aa353b4d8b74920b9a
unknown <elianmontenegro491@gmail.com>
2021-09-19 15:30:16 -0300

    test: ensure Encript return true or false

--- src/utils/Encrypter.spec.ts
+++ src/utils/Encrypter.spec.ts
@@ -1,13 +1,42 @@
+  
+let bcrypt_result : boolean;
+let bcrypt_value : string;
+let bcrypt_hash : string;
+jest.mock('bcrypt', () => ({
+    async compare (value: string, hash: string) {
+        bcrypt_value = value
+        bcrypt_hash = hash
+        return bcrypt_result
+    }
+}))
+
+const bcrypt = require('bcrypt')
 import { Encrypter } from './Encrypter'
 
+const makeSut = () => {
+    return new Encrypter()
+  }
+
 describe('Encrypter', () => {
-    test('should return value hashed and compare value',async () => {
-        const sut = new Encrypter()
-        const password = "any_password"
-        const salt = 10
-        const hashValue = await sut.hash(password, salt);
-        const isValid = await sut.compare(password, hashValue);
+    test('Should return true if bcrypt returns true', async () => {
+        const sut = makeSut()
+        bcrypt_result = true
+        const isValid = await sut.compare('any_value', 'hashed_value')
         expect(isValid).toBe(true)
     })
+    test('Should return false if bcrypt returns false', async () => {
+        const sut = makeSut()
+        bcrypt_result = false
+        const isValid = await sut.compare('any_value', 'hashed_value')
+        expect(isValid).toBe(false)
+    })
     
-}) //TOY PROGRAMANDO CAPA
+    test('Should call bcrypt with correct values', async () => {
+        const sut = makeSut()
+        await sut.compare('any_value', 'hashed_value')
+        expect(bcrypt_value).toBe('any_value')
+        expect(bcrypt_hash).toBe('hashed_value')
+      })
+})
+
+
